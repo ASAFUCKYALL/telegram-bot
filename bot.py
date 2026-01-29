ebd3f1145167985574403ba26a265986f222978d
A$AA <julparme@gmail.com>
2026-01-30 00:16:29 +0100

    Implement error handling for missing BOT_TOKEN
    Added error handling for missing BOT_TOKEN.

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
 from telegram import Update
 from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
 import os
+import sys
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
-async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.chat_join_request.approve()
-
-app = Application.builder().token(TOKEN).build()
-app.add_handler(ChatJoinRequestHandler(approve))
-
-print("Bot actif (GitHub Actions)")
-app.run_polling()
+if not TOKEN:
+    print("ERREUR : BOT_TOKEN manquant")
+    sys.exit(1)
